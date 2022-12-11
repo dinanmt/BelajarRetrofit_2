515b335b72c5a2ec254505255d32cd254f8ea903
Dinan Mutamaddin <dinanmutamaddin@gmail.com>
2022-12-11 12:20:24 +0700

    akses api

--- app/src/main/java/com/example/retrofit_2/network/NewsAPIService.kt
+++ app/src/main/java/com/example/retrofit_2/network/NewsAPIService.kt
@@ -12,8 +12,9 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .build()
 
+
 interface NewsAPIService {
-//    @GET("everything?q=tesla&from=2022-10-15&sortBy=publishedAt&apiKey=9da2ec6a15634ea09aab3cc7868c552e")
+//   @GET("everything?q=tesla&from=2022-10-15&sortBy=publishedAt&apiKey=9da2ec6a15634ea09aab3cc7868c552e")
     @GET("top-headlines?country=us&apiKey=2a78c5b43e5847ac8f95fc1d5fa310d1")
     suspend fun getEverything(): NewsData
 
